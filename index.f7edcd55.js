487cace4f375eea2fdf6f88c2bc90b36f979d8b0
Djoult <55453354+Djoult@users.noreply.github.com>
2022-12-22 17:32:54 +0000

    Deploying to gh-pages from @ Djoult/team17-proj@64bdf0dd93669fcbf6e4a1cb0328a79548d38cf5 🚀

--- index.f7edcd55.js
+++
@@ -1,2 +0,0 @@
-new Swiper(".swiper-container",{pagination:{el:".swiper-pagination",clickable:!0}});
-//# sourceMappingURL=index.f7edcd55.js.map
---
+++ index.ffc1b78e.js
@@ -0,0 +1,2 @@
+new Swiper(".reviews__swiper",{pagination:{el:".swiper-pagination",clickable:!0}});
+//# sourceMappingURL=index.ffc1b78e.js.map
