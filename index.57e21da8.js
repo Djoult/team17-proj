6d613b7186191929bd64e45a0d6b2d916659e4c3
Djoult <55453354+Djoult@users.noreply.github.com>
2022-12-24 20:34:27 +0000

    Deploying to gh-pages from @ Djoult/team17-proj@8611b0fe40633393f83dbad28d84051ecae8b17b 🚀

--- index.57e21da8.js
+++
@@ -1,2 +0,0 @@
-!function(){var e={openModalBtn:document.querySelector("[data-modal-open]"),closeModalBtn:document.querySelector("[data-modal-close]"),modal:document.querySelector("[data-modal]")};function o(){e.modal.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)}();
-//# sourceMappingURL=index.57e21da8.js.map
