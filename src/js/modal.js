ead6e8059e4912db53bc2cdcc1787ebbdf2ff8fd
AlenaRublik <114560252+AlenaRublik@users.noreply.github.com>
2022-12-26 00:13:40 +0200

    qwe

--- src/js/modal.js
+++ src/js/modal.js
@@ -73,17 +73,5 @@
   }
 })();
 
-(() => {
-  const refs = {
-    openModalBtn: document.querySelector('[data-product-modal-open]'),
-    closeModalBtn: document.querySelector('[data-product-modal-close]'),
-    modal: document.querySelector('[data-product-modal]'),
-  };
 
-  refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
 
-  function toggleModal() {
-    refs.modal.classList.toggle('is-hidden');
-  }
-})();
