621cf880ae0bf4cd2d27c56176274752c855a6c8
AlenaRublik <114560252+AlenaRublik@users.noreply.github.com>
2022-12-26 00:15:01 +0200

    add script product section modal

--- src/js/modal.js
+++ src/js/modal.js
@@ -73,5 +73,18 @@
   }
 })();
 
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[data-products-modal-open]'),
+    closeModalBtn: document.querySelector('[data-products-modal-close]'),
+    modal: document.querySelector('[data-products-modal]'),
+  };
 
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+  }
+})();
 
