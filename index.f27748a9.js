99186d5023d8afb0aa39f7e5e5329d14c82d5a9c
Djoult <55453354+Djoult@users.noreply.github.com>
2022-12-28 11:23:53 +0000

    Deploying to gh-pages from @ Djoult/team17-proj@2bf3f1caba9c3ac2ec915e507e58af8e011c0733 🚀

---
+++ index.f27748a9.js
@@ -0,0 +1,2 @@
+var btnUp={el:document.querySelector(".btn-up"),show:function(){this.el.classList.remove("btn-up_hide")},hide:function(){this.el.classList.add("btn-up_hide")},addEventListener:function(){var e=this;window.addEventListener("scroll",(function(){(window.scrollY||document.documentElement.scrollTop)>400?e.show():e.hide()})),document.querySelector(".btn-up").onclick=function(){window.scrollTo({top:0,left:0,behavior:"smooth"})}}};btnUp.addEventListener();
+//# sourceMappingURL=index.f27748a9.js.map
